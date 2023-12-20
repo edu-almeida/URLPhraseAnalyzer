76df7470ce784d61e5143949740ca6e0febfbe2e
Eduard Fronuti <60547531+edu-almeida@users.noreply.github.com>
2023-12-20 12:30:40 -0300

    Initial commit

---
+++ src/main/java/com/desafiosillion/util/Analisador.java
@@ -0,0 +1,22 @@
+package com.desafiosillion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Analisador {
+    public Map<String, Integer> contarPalavras(String conteudo, String frase) {
+        Map<String, Integer> contagemPalavras = new HashMap<>();
+        String[] palavrasFrase = frase.split("\\s+");
+
+        for (String palavra : palavrasFrase) {
+            int ocorrencias = conteudo.split("\\b" + palavra + "\\b", -1).length - 1;
+            contagemPalavras.put(palavra, ocorrencias);
+        }
+
+        return contagemPalavras;
+    }
+
+    public int contarOcorrencias(String conteudo, String frase) {
+        return conteudo.split("\\b" + frase + "\\b", -1).length - 1;
+    }
+}
---
+++ src/main/java/com/desafiosillion/util/Executor.java
@@ -0,0 +1,40 @@
+package com.desafiosillion;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
+
+public class Executor {
+    public void executar(String url, String frase) {
+        String conteudoPagina = realizarSolicitacaoHTTP(url);
+
+        // Criar instância da classe Analisador e chamar os métodos
+        Analisador analisador = new Analisador();
+        int ocorrenciasFrase = analisador.contarOcorrencias(conteudoPagina, frase);
+        Map<String, Integer> contagemPalavras = analisador.contarPalavras(conteudoPagina, frase);
+
+        // Exibir resultados no console
+        System.out.println("\"" + frase + "\" => repete " + ocorrenciasFrase + " vezes");
+
+        for (Map.Entry<String, Integer> entry : contagemPalavras.entrySet()) {
+            System.out.println("\"" + entry.getKey() + "\" => é repetido " + entry.getValue() + " vezes");
+        }
+    }
+
+    private String realizarSolicitacaoHTTP(String url) {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
